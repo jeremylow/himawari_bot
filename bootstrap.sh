e9e25a6252ae3318be32e19e2a1a2f81c52b90e5
Jeremy Low <jeremylow@gmail.com>
2015-11-07 18:52:49 -0500

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ mkdir hires
 mkdir lowres
 mkdir videos
 
-pip3 install -r requirements.txt
+pip3 install -r /data/himawari_bot/requirements.txt
 
 ln -s himawari_cron /etc/cron.d/himawari_cron
 chmod 0644 /etc/cron.d/himawari_cron
