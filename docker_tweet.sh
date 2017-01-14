596da2d994a88ff3cb59337d8a9050e916da3ee1
Jeremy Low <jeremylow@gmail.com>
2017-01-14 06:44:11 -0500

    delete old docker stuff

--- docker_tweet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -d -v /home/ubuntu/apps/himawari/config.py:/data/himawari_bot/config.py -v /home/ubuntu/apps/himawari/hires.log:/data/himawari_bot/hires.log -v /home/ubuntu/apps/himawari/hires:/data/himawari_bot/hires jeremylow/himawari2:latest cron -f
