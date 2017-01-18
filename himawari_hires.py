8b613e8e0f6730f2e592a7eac3bea013ee7fabb1
Jeremy Low <jeremylow@gmail.com>
2017-01-18 07:43:14 -0500

    better hires logging facility

--- himawari_hires.py
+++ himawari_hires.py
@@ -9,7 +9,7 @@
 from __future__ import print_function, unicode_literals, absolute_import
 
 import os
-from os.path import abspath, dirname, join
+from os.path import abspath, dirname, join, getsize
 
 import imghdr
 import logging
@@ -62,7 +62,7 @@ EARTH_POLYGON = MultiPoint(POINTS).convex_hull
 
 def set_up_logging(level=logging.DEBUG):
     global logger
-    logger = logging.getLogger('HiResLogger')
+    logger = logging.getLogger(__name__)
     logger.setLevel(level)
 
     handler = logging.handlers.RotatingFileHandler(LOGFILE,
@@ -76,6 +76,7 @@ def set_up_logging(level=logging.DEBUG):
 
 def get_api():
     """Returns an authenticated Twitter API instance"""
+    logger.info('Getting API')
     api = twitter.Api(
         config.CONSUMER_KEY,
         config.CONSUMER_SECRET,
@@ -86,6 +87,7 @@ def get_api():
     logger.debug('Using Api: %s', api)
     return api
 
+
 def get_start_coord():
     """
     Get a top-left point to start our downward-rightward crop that
@@ -94,7 +96,7 @@ def get_start_coord():
     Returns:
         coordinate tuple (0, 0 being top, left)
     """
-    logger.debug("Getting coordinates")
+    logger.info("Getting coordinates")
     while True:
         try_point = Point(random.randint(1, 4219), random.randint(732, 5499))
         if try_point.within(EARTH_POLYGON):
@@ -105,7 +107,7 @@ def get_start_coord():
     # since Shapely counts up from bottom right and PIL counts
     # down from top left.
     lng_px, lat_px = (int(try_point.x), int(5500 - try_point.y))
-    logger.debug("Using coordinates: (%d, %d)", lat_px, lng_px)
+    logger.debug("Using coordinates: (%s, %s)", lat_px, lng_px)
     return (lat_px, lng_px)
 
 
@@ -121,7 +123,7 @@ def crop_hires_images(images, lat_start=None, lng_start=None):
     Returns:
         list of cropped images
     """
-    logger.debug("Cropping images")
+    logger.info("Cropping images")
     width, height = 720, 720
     top, left = lat_start, lng_start
 
@@ -132,6 +134,7 @@ def crop_hires_images(images, lat_start=None, lng_start=None):
         # If imghdr can't ID the file, then it's probably corrupt and we'll
         # just drop that frame and delete the file.
         if not imghdr.what(filename):
+            logger.debug("Deleting %s", filename)
             os.remove(filename)
             continue
 
@@ -141,8 +144,8 @@ def crop_hires_images(images, lat_start=None, lng_start=None):
             crop_fn = "img{0}.png".format(str(idx).zfill(3))
             im2.save(join(BASE_DIR, crop_fn))
         except Exception as e:
-            logger.debug("Failed to crop image.")
-            logger.debug(str(e))
+            logger.error("Failed to crop image.", exc_info=True)
+            continue
 
 
 def get_cira_images(num=60):
@@ -152,7 +155,7 @@ def get_cira_images(num=60):
     Returns:
         files (list): List of urls to download
     """
-    logger.debug("Fetching images")
+    logger.info("Fetching images")
     page_content = requests.get(CIRA_LIST_URL)._content
     soup = BeautifulSoup(page_content, 'html.parser')
     links = soup.find_all('a', string=re.compile('^Hi-Res Image'), limit=num)
@@ -160,21 +163,26 @@ def get_cira_images(num=60):
     image_urls = [link.attrs['href'] for link in links]
     prev_downloaded_images = os.listdir(HIRES_FOLDER)
 
-    for image in image_urls:
+    for idx, image in enumerate(image_urls):
+        logger.debug("Getting (%s / %s) image: %s", idx + 1, num, image)
         image_name = os.path.basename(image)
         full_image_url = "{0}{1}".format(CIRA_IMG_BASE_URL, image)
+        download_name = join(HIRES_FOLDER, image_name)
 
-        if image_name in prev_downloaded_images:
+        if image_name in prev_downloaded_images and getsize(download_name) > 1024:
             # Don't redownload images we already have.
+            logger.debug("Skipping previously downloaded image %s", image_name)
             continue
 
-        with open(join(HIRES_FOLDER, image_name), 'wb') as f:
+        with open(download_name, 'wb') as f:
             data = requests.get(full_image_url)
             f.write(data._content)
+            logger.debug("Successfully downloaded %s", full_image_url)
 
         # If the image is smaller than 1024 bytes, it's one of the broken ones
-        if os.path.getsize(join(HIRES_FOLDER, image_name)) < 1024:
-            os.remove(join(HIRES_FOLDER, image_name))
+        if getsize(download_name) < 1024:
+            logger.debug("Deleting corrupt image %s", image_name)
+            os.remove(download_name)
 
 
 def delete_old_cira_images(num=60):
@@ -182,16 +190,17 @@ def delete_old_cira_images(num=60):
     Args:
         num (int, optional): number of images to retain on the server
     """
-    logger.debug("Deleting old CIRA images")
+    logger.info("Deleting old CIRA images")
     images = sorted(os.listdir(HIRES_FOLDER))
 
     num_images_to_del = len(images) - num
-    logger.debug("Deleting %d images", num_images_to_del)
+    logger.debug("Deleting %s images", num_images_to_del)
 
     if num_images_to_del <= 0:
         return True
 
     for img in images[:abs(num_images_to_del)]:
+        logger.debug("Deleting %s", img)
         os.remove(join(HIRES_FOLDER, img))
 
 
@@ -200,7 +209,7 @@ def refresh_images(num=60):
     Args:
         num (int, optional): number of images to end up with.
     """
-    logger.debug("Refreshing images")
+    logger.info("Refreshing images")
     get_cira_images(num=num)
     delete_old_cira_images(num=num)
 
@@ -213,7 +222,7 @@ def make_hires_animation(lat_start=None, lng_start=None):
     Returns:
         (tuple): Coordinates, path for MP4
     """
-    logger.debug("Making hi-res video")
+    logger.info("Making hi-res video")
 
     images = os.listdir(HIRES_FOLDER)
     out = join(BASE_DIR, "video_out.mp4")
@@ -225,21 +234,21 @@ def make_hires_animation(lat_start=None, lng_start=None):
     coordinates = geometry.px_to_lat_long(lat_start+360, lng_start+360)
 
     cmd = "{0}/hires_mp4.sh {1} {2}".format(BASE_DIR, BASE_DIR, out)
-    logger.debug(cmd)
+    logger.debug("Hires command: %s", cmd)
     subprocess.call("{0}/hires_mp4.sh {1} {2}".format(BASE_DIR, BASE_DIR, out), shell=True)
     mp4_path = os.path.realpath(out)
     return (coordinates, mp4_path)
 
 
 def tweet_video(coordinates=None, mp4=None):
-    logger.debug("Starting tweet")
+    logger.info("Starting tweet")
 
     if not mp4:
-        refresh_images(num=75)
+        refresh_images(num=55)
         coordinates, mp4 = make_hires_animation()
 
     short_link = short_osm(coordinates[0], coordinates[1], zoom=6, marker=True)
-    logger.debug(short_link)
+    logger.info(short_link)
 
     try:
         api = get_api()
@@ -247,9 +256,9 @@ def tweet_video(coordinates=None, mp4=None):
             status="Coordinates: {0}; {1}".format(str(coordinates), short_link),
             media=mp4)
     except Exception as e:
-        logger.exception(str(e))
+        logger.error("Failed to tweet", exc_info=True)
     os.remove(mp4)
-    logger.debug("Finished tweet")
+    logger.info("Finished tweet")
 
 
 if __name__ == '__main__':
